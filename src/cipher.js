5f93189a9a184120a45f79e5ea246036577563f2
marali5 <mara_lilian@hotmail.com>
2019-02-10 13:19:33 -0600

    producto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,22 @@
 
 window.cipher = {
 
-encode :(n, message)=>{ 
+encode :(offset, message)=>{ 
   let string = [];
   let code = [];
   for (let i=0; i < message.length; i++) { 
-   string.push((message.charCodeAt(i)-65+n)%26+65); 
+   string.push((message.charCodeAt(i)-65+offset)%26+65); 
     code.push(String.fromCharCode(string[i])); 
       }
       let newCode = code.join("");
       return newCode;
 
 },
-decode :(n, message)=>{ 
+decode :(offset, message)=>{ 
   let string = [];
   let uncode = [];
   for(let i = 0; i < message.length; i++){ 
-  string.push((message.charCodeAt(i)+65-n)%26+65); 
+  string.push((message.charCodeAt(i)+65-offset)%26+65); 
   uncode.push(String.fromCharCode(string[i])); 
       }
       let newCode = uncode.join("");
--- src/index.js
+++ src/index.js
@@ -1,31 +1,53 @@
+/*boton option1 screen1 que envia a screen2*/
+document.getElementById("option1").addEventListener("click", functionTwo);
 function functionTwo (){
- document.getElementById("pantalla1").style.display="none";
- document.getElementById("pantalla2").style.display="block";
+	document.getElementById("screen1").style.display="none";
+	document.getElementById("screen2").style.display="block";
 }
-document.getElementById("opcion1").addEventListener("click", functionTwo);
-
+/*boton option2 screen1 que envia a screen3*/
+document.getElementById("option2").addEventListener("click", functionThree);
 function functionThree(){
-  document.getElementById("pantalla1").style.display="none";
-  document.getElementById("pantalla3").style.display="block";
+  document.getElementById("screen1").style.display="none";
+  document.getElementById("screen3").style.display="block";
 }
-document.getElementById("opcion2").addEventListener("click", functionThree);
 
-
+/*boton screen2 que codifica*/
 document.getElementById("submit").addEventListener("click",encodeBtnn);
-
 function encodeBtnn(){
 let message=document.getElementById("texto").value; 
-	let n=parseInt(document.getElementById("clave").value); 
-	let result= cipher.encode(n,message);
+let offset=parseInt(document.getElementById("clave").value); 
+let result= cipher.encode(offset,message);
 	document.getElementById("cifrado").innerHTML = (result.toUpperCase());
-
 }
 
+/*boton screen3 que decodifica*/
 document.getElementById("submit2").addEventListener("click",decodeBtnn);
-
 function decodeBtnn(){
 let message=document.getElementById("texto2").value; 
- let n=parseInt(document.getElementById("clave2").value); 
- let result= cipher.decode(n,message);
+ let offset=parseInt(document.getElementById("clave2").value); 
+ let result= cipher.decode(offset,message);
  document.getElementById("descifrado").innerHTML = (result.toUpperCase());
 }
+
+/*boton option3  que envia a screen2*/
+document.getElementById("option3").addEventListener("click", functionFour);
+function functionFour (){
+document.getElementById("screen2").style.display="none";
+	document.getElementById("screen1").style.display="block";
+	cleanValues ();
+}
+
+document.getElementById("option4").addEventListener("click", functionFive);
+function functionFive (){
+document.getElementById("screen3").style.display="none";
+	document.getElementById("screen1").style.display="block";
+	cleanValues ();
+}
+
+const cleanValues = () =>{
+    document.getElementById('clave').value = '';
+    document.getElementById('texto').value = '';
+    document.getElementById('clave2').value = '';
+    document.getElementById('texto2').value = '';
+
+};
